bdcde2e97895c9bc8652b204c45b9bc5599f81a6
Mohammad Chand Alam <amuchand47@gmail.com>
2022-09-25 17:57:34 +0530

    Success Alert Added

--- CWH/21_Get_Post.php
+++ CWH/21_Get_Post.php
@@ -57,7 +57,7 @@
         $email = $_POST['email'];
         $password = $_POST['pass'];
 
-        echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+        echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
          <strong>Success!</strong> Your '.$email.' and '.$password.' registered successfully! 
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
